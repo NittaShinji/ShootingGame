9a8d92962e2bf0ba6dd31004c58c3b8d553d0560
Nittshinji <94505316+NittaShinji@users.noreply.github.com>
2022-06-14 16:39:11 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //プレイヤーのワールド座標を取得
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        //左矢印キーが入力されたとき
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //右方向に0.01動く
+            pos.x += 0.01f;
+        }
+        //左矢印キーが入力されたとき
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //右方向に0.01動く
+            pos.x -= 0.01f;
+        }
+        //上矢印キーが入力されたとき
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            //右方向に0.01動く
+            pos.z += 0.01f;
+        }
+        //左矢印キーが入力されたとき
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            //右方向に0.01動く
+            pos.z -= 0.01f;
+        }
+
+        transform.position = new Vector3(pos.x, pos.y, pos.z);
+    }
+}
